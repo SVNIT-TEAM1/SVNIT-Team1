934999bccedaac5fd5ba1cb3b4834c73b12bdeb1
heli576 <helivakharia@gmail.com>
2021-08-31 18:23:37 +0530

    Add search components

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import Dashboard from './components/Dashboard';
 
 function App() {
+  document.body.style.backgroundColor = "#f8f9fa";
   return (
     <div style={{padding:35}}>
     <Dashboard/>
     </div>
-  );
+  )
 }
 
 export default App;
--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
+import axios from "axios";
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import Search from "./Search";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
   paper: {
-    padding: theme.spacing(2),
+    padding: 25,
     textAlign: 'center',
     color: theme.palette.text.secondary,
   },
@@ -16,12 +18,36 @@ const useStyles = makeStyles((theme) => ({
 
 export default function CenteredGrid() {
   const classes = useStyles();
+  const [symbol,setSymbol]=useState("AAPL");
+  const [desc, setDesc]=useState("");
+  const [error,setError]=useState(false);
+
+  const getDescription = async() =>{
+    try{
+      const resp = await axios.get(`https://cloud.iexapis.com/stable/stock/${symbol}/company?token=pk_b8deb498d27c4f6ab328db52163822a7`);
+
+        console.log(resp.data);
+        setDesc(resp.data);
+    }catch(error){
+      setError("Something went wrong");
+    }
+  }
+
+  useEffect(()=>{
+    getDescription();
+  },[])
 
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs={8}>
-          <Paper className={classes.paper}>Search, Chart</Paper>
+          <Paper className={classes.paper}>
+          <Grid container>
+          <Grid item xs={3}></Grid>
+          <Grid item xs={8}><Search setSymbol={setSymbol} getDescription={getDescription}/></Grid>
+          <Grid item xs={1}></Grid>
+          </Grid>
+          </Paper>
         </Grid>
         <Grid item xs={4}>
           <Paper className={classes.paper}>History</Paper>
