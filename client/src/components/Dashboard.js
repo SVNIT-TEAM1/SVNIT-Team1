ab0c7767ed8e3c14b84b7a66f81452252abbdd59
UMuktesh <u19cs080@coed.svnit.ac.in>
2021-08-31 18:31:37 +0530

    .

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,7 +1,11 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+
+import Description from "./Description";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -9,13 +13,40 @@ const useStyles = makeStyles((theme) => ({
   },
   paper: {
     padding: theme.spacing(2),
-    textAlign: 'center',
+    textAlign: "center",
     color: theme.palette.text.secondary,
   },
 }));
 
 export default function CenteredGrid() {
   const classes = useStyles();
+  const [symbol, setSymbol] = useState("AAPL");
+  const [data, setData] = useState({});
+
+  const onSearch = (sym) => {
+    axios
+      .get(
+        "https://cloud.iexapis.com/stable/stock/" +
+          sym +
+          "/company?token=pk_b8deb498d27c4f6ab328db52163822a7"
+      )
+      .then(function (response) {
+        // handle success
+        console.log(response);
+        setData(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+  };
+
+  useEffect(() => {
+    onSearch(symbol);
+  }, [symbol]);
 
   return (
     <div className={classes.root}>
@@ -25,8 +56,8 @@ export default function CenteredGrid() {
         </Grid>
         <Grid item xs={4}>
           <Paper className={classes.paper}>History</Paper>
+          <Description data={data} />
         </Grid>
-
       </Grid>
     </div>
   );
---
+++ client/src/components/Description.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+const Description = (props) => {
+  <>
+    <h4>
+      <a href={props.website}>{props.companyName}</a>
+    </h4>
+    <p>{props.description}</p>
+  </>;
+};
+
+export default Description;
