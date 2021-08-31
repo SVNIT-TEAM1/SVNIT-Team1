c14df69dcb354bda589d3e104bc7e1bd19096072
UMuktesh <u19cs080@coed.svnit.ac.in>
2021-08-31 23:37:05 +0530

    Added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,7 @@ import Dashboard from './components/Dashboard';
 function App() {
   document.body.style.backgroundColor = "#f8f9fa";
   return (
-    <div style={{padding:35}}>
     <Dashboard/>
-    </div>
   )
 }
 
